e9fd3cfb26e62199e88ea6df3dad73d2a51df57b
raynlamlaika <raynlamlaika@users.noreply.github.com>
2025-12-15 13:58:54 +0100

    easy ex 00 as he look just for syntax familer

---
+++ src/main/module00/ex00/program.java
@@ -0,0 +1,43 @@
+
+
+
+
+
+// first the stander stream of the data we gonna use is System.out to put our data
+
+//System.out.print("Hello "); hmmmm why we need class to run this
+
+
+
+
+public class Main {
+    public static int counter(String dataholder) {
+        int sum = 0;
+        for (int i = 0; i < dataholder.length()  ; i++)
+        {
+            boolean o = Character.isDigit(dataholder.charAt(i));
+            char c =  dataholder.charAt(i);
+            int conv =  c - '0';
+            if (o)
+                sum += conv;
+            else
+            {
+                System.out.print("ins't valid sicuance of number\n");
+                return 0;
+            }
+
+        }
+        return sum; // Return the result
+    }
+    public static void main(String[] args) {
+        // i willl do the work for both str and int
+
+        // with var compiler he will cast it by his self
+//        var dataholder =  "479598";
+        var intholder = 479598;
+        String str = String.valueOf(intholder);
+        //check all ints
+        int sum = counter(str);
+        System.out.print(sum);
+    }
+}
